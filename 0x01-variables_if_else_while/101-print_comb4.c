fc1dbc15899ac51667aeb05b05987e22f92b3c98
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-04-12 01:25:41 -0700

    print comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,20 +15,22 @@ int main(void)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
-			       	if (c < i && i < k)
-			      {
-																									putchar(c);
-																									putchar(i);
-																									putchar(k);
-																									if (c != '7')		
-																									{
-																										putchar(',');
-																										putchar(' ');
-																									}
-			      }
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
