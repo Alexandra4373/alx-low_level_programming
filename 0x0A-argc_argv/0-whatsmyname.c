871602d691b55e23fe8c1135e164c0e5739d58ee
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-05-04 22:54:44 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the number of arguments passed into it
- * @argc: number of arguments
- * @argv: array of pointers
+ * main - Entry point
  *
- * Return: 0
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
  */
 
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char **argv)
 {
-	printf("%d\n", argc - 1);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
