4ec6159f54a90a2703bdb89049cbb36d0dd6f956
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-05-26 14:01:05 -0700

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,59 +7,35 @@
  *
  * Return: number of elements in the freed list
  */
-listint_t *find_listint_loop_fl(listint_t *head)
-{
-	listint_t *ptr, *end;
-
-	if (head == NULL)
-		return (NULL);
-
-	for (end = head->next; end != NULL; end = end->next)
-	{
-		if (end == end->next)
-			return (end);
-		for (ptr = head; ptr != end; ptr = ptr->next)
-			if (ptr == end->next)
-				return (end->next);
-	}
-	return (NULL);
-}
-
-/**
- * free_listint_safe - frees a listint list, even if it has a loop
- *
- * @h: head of list
- *
- * Return: number of nodes freed
- */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *next, *loopnode;
-	size_t len;
-	int loop = 1;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-	if (h == NULL || *h == NULL)
+	if (!h || !*h)
 		return (0);
 
-	loopnode = find_listint_loop_fl(*h);
-	for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
+	while (*h)
 	{
-		len++;
-		next = (*h)->next;
-		if (*h == loopnode && loop)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
-			if (loopnode == loopnode->next)
-			{
-				free(*h);
-				break;
-			}
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
 			len++;
-			next = next->next;
-			free((*h)->next);
-			loop = 0;
 		}
-		free(*h);
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
 	}
+
 	*h = NULL;
+
 	return (len);
 }
