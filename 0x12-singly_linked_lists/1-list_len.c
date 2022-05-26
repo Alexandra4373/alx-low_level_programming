031c55896bbe9216e92c7a7197492b19c189e10b
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-05-26 14:12:14 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,12 +10,14 @@
  *      */
 size_t list_len(const list_t *h)
 {
-	size_t i = 0;
+	size_t list_len(const list_t *h)
+{
+	int nodes = 0;
 
 	while (h)
 	{
-		i++;
+		nodes += 1;
 		h = h->next;
 	}
-	return (i);
+	return (nodes);
 }
