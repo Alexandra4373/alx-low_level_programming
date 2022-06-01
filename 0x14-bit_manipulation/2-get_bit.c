010ab42f0bcd61b1f4ca2092c5d0941d21d4f4a8
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-06-01 08:00:04 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    int bit_val;
-
-    if (index > 63)
-        return (-1);
-
-    bit_val = (n >> index) & 1;
-
-    return (bit_val);
+	if (index > sizeof(unsigned long int) * 8 - 1)
+		return (-1);
+	return ((n >> index) & 1);
 }
