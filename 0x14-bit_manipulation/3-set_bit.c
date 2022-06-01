f187dfacda20387e8088ccdebbf9036dae13f7da
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-06-01 08:01:53 -0700

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index > 63)
-        return (-1);
-
-    *n = ((1UL << index) | *n);
-    return (1);
+	if (n == NULL || (index > sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	*n = (*n) | (1 << index);
+	return (1);
 }
