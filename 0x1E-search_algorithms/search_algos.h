7cc17c791153aaa7d57d204d3a79df7a988268a7
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-11-02 08:43:41 -0700

    header files/prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,5 +12,6 @@ void array_print(int *array, size_t lo, size_t hi);
 int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
